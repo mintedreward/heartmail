85eab793dc490e4aef4655ae3940c38070cc4ce6
Ryan X. Charles <ryan@coasian.com>
2021-10-12 10:27:27 -0700

    update open source tool

--- js/openspv/bin/openspv.js
+++ js/openspv/bin/openspv.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict'
 const repl = require('repl')
-const spv = require('../')
+const OpenSPV = require('../')
 // Make all SPV classes globally available.
-Object.assign(global, spv, { spv: spv })
+Object.assign(global, OpenSPV, { OpenSPV: OpenSPV })
 repl.start({
-  prompt: 'spv> ',
+  prompt: 'OpenSPV> ',
   useGlobal: true,
   ignoreUndefined: true
 })
---
+++ update-open-source-repos.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# tool to update all external open source repos that we control from source
+# code contained inside the master repo. for now, this means just the OpenSPV
+# project.
+
+# make sure we are in the coasian source directory
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+# assume openspv is in the same containing folder as coasian
+# delete all non-hidden files in that repo
+rm -rf ../openspv/*
+
+# copy all files into external repo
+rsync -av --progress js/openspv/* ../openspv --exclude node_modules
+
+# commit changes and push to github
+cd ../openspv
+git add .
+git commit -m "commit"
+git push origin master
