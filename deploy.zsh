65d0be152f047cb751311587006c1a1406282353
Ryan X. Charles <ryan@coasian.com>
2021-10-14 09:14:32 -0700

    testing deployment

--- deploy.zsh
+++ deploy.zsh
@@ -4,32 +4,43 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 
+# load environment variables for production
+source .env.prod
+
+# publish latest version to npm
 lerna publish
 
+# get the latest verision number we just created
 version=`git describe --tags --exact-match`
 
+# deploy coasian-web
 cd $dir/js/coasian-web
 echo Building coasian-web
 docker build --build-arg NPM_TOKEN=${NPM_TOKEN} . -t coasian-web
 docker tag coasian-web ryanxcharles/coasian-web:${version}
 docker push ryanxcharles/coasian-web:${version}
 echo Deploying coasian-web
+ssh -t coasian-web-1 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t coasian-web-1 'docker kill $(docker ps -q)'
 ssh -t coasian-web-1 'docker rm $(docker ps -a -q)'
 ssh -t coasian-web-1 "docker run --detach -p 80:3000 ryanxcharles/coasian-web:${version}"
+ssh -t coasian-web-2 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t coasian-web-2 'docker kill $(docker ps -q)'
 ssh -t coasian-web-2 'docker rm $(docker ps -a -q)'
 ssh -t coasian-web-2 "docker run --detach -p 80:3000 ryanxcharles/coasian-web:${version}"
 
+# deploy openspv-web
 cd $dir/js/openspv-web
 echo Building openspv-web
 docker build --build-arg NPM_TOKEN=${NPM_TOKEN} . -t openspv-web
 docker tag openspv-web ryanxcharles/openspv-web:${version}
 docker push ryanxcharles/openspv-web:${version}
 echo Deploying openspv-web
+ssh -t openspv-web-1 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t openspv-web-1 'docker kill $(docker ps -q)'
 ssh -t openspv-web-1 'docker rm $(docker ps -a -q)'
 ssh -t openspv-web-1 "docker run --detach -p 80:3000 ryanxcharles/openspv-web:${version}"
+ssh -t openspv-web-2 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t openspv-web-2 'docker kill $(docker ps -q)'
 ssh -t openspv-web-2 'docker rm $(docker ps -a -q)'
 ssh -t openspv-web-2 "docker run --detach -p 80:3000 ryanxcharles/openspv-web:${version}"
