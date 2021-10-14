3c3a74b56c4003c7c1233a09432d343f9b3ba103
Ryan X. Charles <ryan@coasian.com>
2021-10-14 09:21:26 -0700

    testing deployment

--- deploy.zsh
+++ deploy.zsh
@@ -20,11 +20,11 @@ docker build --build-arg NPM_TOKEN=${NPM_TOKEN} . -t coasian-web
 docker tag coasian-web ryanxcharles/coasian-web:${version}
 docker push ryanxcharles/coasian-web:${version}
 echo Deploying coasian-web
-ssh -t coasian-web-1 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
+ssh -t coasian-web-1 "echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin"
 ssh -t coasian-web-1 'docker kill $(docker ps -q)'
 ssh -t coasian-web-1 'docker rm $(docker ps -a -q)'
 ssh -t coasian-web-1 "docker run --detach -p 80:3000 ryanxcharles/coasian-web:${version}"
-ssh -t coasian-web-2 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
+ssh -t coasian-web-2 "echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin"
 ssh -t coasian-web-2 'docker kill $(docker ps -q)'
 ssh -t coasian-web-2 'docker rm $(docker ps -a -q)'
 ssh -t coasian-web-2 "docker run --detach -p 80:3000 ryanxcharles/coasian-web:${version}"
@@ -36,10 +36,12 @@ docker build --build-arg NPM_TOKEN=${NPM_TOKEN} . -t openspv-web
 docker tag openspv-web ryanxcharles/openspv-web:${version}
 docker push ryanxcharles/openspv-web:${version}
 echo Deploying openspv-web
+ssh -t openspv-web-1 "echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin"
 ssh -t openspv-web-1 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t openspv-web-1 'docker kill $(docker ps -q)'
 ssh -t openspv-web-1 'docker rm $(docker ps -a -q)'
 ssh -t openspv-web-1 "docker run --detach -p 80:3000 ryanxcharles/openspv-web:${version}"
+ssh -t openspv-web-2 "echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin"
 ssh -t openspv-web-2 'docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD'
 ssh -t openspv-web-2 'docker kill $(docker ps -q)'
 ssh -t openspv-web-2 'docker rm $(docker ps -a -q)'
