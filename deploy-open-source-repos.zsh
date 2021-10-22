d1207a4b94f679f1c2117acd736aac61004b0336
Ryan X. Charles <ryan@coasian.com>
2021-10-22 09:37:42 -0700

    deploy script

--- deploy-open-source-repos.zsh
+++ deploy-open-source-repos.zsh
@@ -8,17 +8,22 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 
+# make sure we get all subtrees
+git submodule update --init --recursive
+
 # delete all non-hidden files in OpenSPV
 cd $dir/../openspv
 rm -rf ./*
+rm ./.*
 
-# copy all files from npm
+# copy all files from the latest version on npm
 curl `npm view openspv dist.tarball` > openspv.tgz
 tar -xzf openspv.tgz
 mv package/* ./
 mv package/.* ./
 rmdir package
 
+# update on github
 version=`npm view openspv version`
 git add .
 git commit -m "$version"
