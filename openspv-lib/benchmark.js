7123b32efa499c554b21c72e317cbdd415beda58
Ryan X. Charles <ryan@coasian.com>
2021-10-23 08:48:40 -0700

    Squashed 'public/openspv/' content from commit 719459bf
    git-subtree-dir: public/openspv
    git-subtree-split: 719459bf655416b6fdc14889c9382b3d44a463c5

---
+++ openspv-lib/benchmark.js
@@ -0,0 +1,50 @@
+const openspv = require('./dist/spv')
+
+const Address = openspv.Address
+const PrivKey = openspv.PrivKey
+const PubKey = openspv.PubKey
+const TxBuilder = openspv.TxBuilder
+const TxOut = openspv.TxOut
+const Random = openspv.Random
+const Bn = openspv.Bn
+const KeyPair = openspv.KeyPair
+
+const randhex = 'adf4953b2e679fdc453d9cec93ba26c3bd9f0fb875975f3d72ed0c6c6835e26e'
+const randbn = new Bn().fromHex(randhex)
+const privateKey = PrivKey.fromBn(randbn)
+const publicKey = PubKey.fromPrivKey(privateKey)
+const keyPair = new KeyPair(privateKey, publicKey)
+const fromAddress = Address.fromPrivKey(privateKey)
+const toAddress = fromAddress
+const changeAddress = toAddress
+
+const n = 10000
+const satoshis = 1e3
+// const total = satoshis * n - satoshis / 2
+let txb = new TxBuilder()
+for (let i = 0; i < n; i++) {
+  const txOut = TxOut.fromProperties(new Bn(satoshis), fromAddress.toTxOutScript())
+  const txHashBuf = Random.getRandomBuffer(32)
+  const txOutNum = 0
+  txb.inputFromPubKeyHash(txHashBuf, txOutNum, txOut, publicKey)
+}
+txb = txb.outputToAddress(new Bn(satoshis), toAddress)
+txb = txb.setChangeAddress(changeAddress)
+txb.setFeePerKbNum(500)
+const useAllInputs = true
+
+{
+  const start = Date.now()
+  txb.build({ useAllInputs })
+  const finish = Date.now()
+  console.log('building: ', finish - start, 'ms')
+}
+
+{
+  const start = Date.now()
+  for (let i = 0; i < txb.txIns.length; i++) {
+    txb.signTxIn(i, keyPair)
+  }
+  const finish = Date.now()
+  console.log('', n, 'inputs', 'signing: ', finish - start, 'ms')
+}
