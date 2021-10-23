93ce3753ef9143670282ae0c25134e047b0fa9fa
Ryan X. Charles <ryan@coasian.com>
2021-10-22 17:48:25 -0700

    move to projects directory

--- update-open-source-repos.zsh
+++ update-open-source-repos.zsh
@@ -1,28 +1,27 @@
 #!/bin/zsh
 
-## tool to update all external open source repos that we control from source
-## code contained inside the master repo. for now, this means just the OpenSPV
-## project.
-#
-## make sure we are in the coasian source directory
-#dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-#cd $dir
-#
-## get all open-source repos ("subtree" - currently only OpenSPV)
-#git subtree pull --prefix=open-source/openspv openspv master
-#
-## delete all non-hidden files in OpenSPV
-#cd $dir/open-source/openspv
-#rm -rf ./*
-#rm ./.*
-#
-## copy all files from the latest version on npm
-#curl `npm view openspv dist.tarball` > openspv.tgz
-#tar -xzf openspv.tgz
-#mv package/* ./
-#mv package/.* ./
-#rmdir package
-#
+# tool to update all external open source repos that we control from source
+# code contained inside the master repo. for now, this means just the OpenSPV
+# project.
+
+# make sure we are in the coasian source directory
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
+
+# get all open-source repos ("subtree" - currently only OpenSPV)
+git subtree pull --prefix=open-source/openspv openspv master
+
+# delete all non-hidden files in OpenSPV
+cd $dir/open-source/openspv
+rm -rf ./projects/*
+
+# copy all files from the latest version on npm
+curl `npm view @openspv/lib dist.tarball` > openspv-lib.tgz
+tar -xzf openspv-lib.tgz
+mv package/* ./projects/openspv-lib
+mv package/.* ./projects/openspv-lib
+rmdir package
+
 ## update on github
 #version=`npm view openspv version`
 #git add .
