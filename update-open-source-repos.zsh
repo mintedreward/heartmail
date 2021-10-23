e73f93ff7a474f630248563e1be57b3f4308f8a3
Ryan X. Charles <ryan@coasian.com>
2021-10-23 08:34:32 -0700

    OpenSPV is public

--- update-open-source-repos.zsh
+++ update-open-source-repos.zsh
@@ -3,13 +3,13 @@
 # Sync all public code to external repos by pulling and then pushing. Right now
 # this means just the OpenSPV repo containing all OpenSPV subprojects.
 
-git subtree add --prefix private/openspv git@github.com:openspv/openspv.git master --squash
+git subtree add --prefix public/openspv git@github.com:openspv/openspv.git master --squash
 
 # get all open-source repos ("subtree" - currently only OpenSPV)
-git subtree pull --prefix=private/openspv openspv master
+git subtree pull --prefix=public/openspv openspv master
 
 # update on github
 version=`npm view @openspv/lib version`
 git add .
 git commit -m "$version"
-git subtree push --prefix=private/openspv openspv master
+git subtree push --prefix=public/openspv openspv master
