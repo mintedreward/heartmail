d825d4f7e4f66a9a5a82714a5ec018eb56972bc1
Ryan X. Charles <ryan@coasian.com>
2021-10-23 08:09:44 -0700

    update open source tool

--- update-open-source-repos.zsh
+++ update-open-source-repos.zsh
@@ -11,16 +11,6 @@ cd $dir
 # get all open-source repos ("subtree" - currently only OpenSPV)
 git subtree pull --prefix=open-source/openspv openspv master
 
-# delete all non-hidden files in OpenSPV
-cd $dir/open-source/openspv
-rm -rf ./projects/*
-
-# copy all files from the latest version in the repo
-tarball=`npm pack ../../projects/openspv-lib`
-tar -xzf $tarball
-mv package ./projects/openspv-lib
-rm $tarball
-
 # update on github
 version=`npm view @openspv/lib version`
 git add .
