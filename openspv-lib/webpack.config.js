49b76dd3f9b417eef8772b83d9fc8d4f20ae16f2
Ryan X. Charles <ryan@coasian.com>
2021-10-25 18:49:53 -0700

    OpenSPVLib

--- openspv-lib/webpack.config.js
+++ openspv-lib/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'spv.bundle.js',
-    library: 'spv'
+    filename: 'openspv-lib.bundle.js',
+    library: 'OpenSPVLib'
   },
   devtool: 'source-map',
   mode: 'production'
