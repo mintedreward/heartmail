1cb6f3254e44249a7cf3bff2e15da2d64518a5fe
Ryan X. Charles <ryan@coasian.com>
2021-10-27 13:36:08 -0700

    Keyfile

--- private/openspv/keyfile/src/index.mjs
+++ private/openspv/keyfile/src/index.mjs
@@ -2,45 +2,68 @@
  * Keyfile
  * =======
  *
- * Each user has at least one identity key. Each identity key always belongs to
- * a paymail. Not all paymails are necessarily present in this file. Only unique
- * (non-deterministic) identity keys are present in this file. It is intended
- * that a user should have not too many unique identity keys. Typically the only
- * reason to generate a new identity key is if a key is lost or stolen. However,
- * it is possible the user will choose to rotate keys as a precaution even if no
- * keys have been lost or stolen.
+ * Each user has at least one identity key. Only unique (non-deterministic)
+ * identity keys are present in this file. It is intended that a user should
+ * have not too many unique identity keys. Typically the only reason to generate
+ * a new identity key is if a key is lost or stolen. However, it is possible the
+ * user will choose to rotate keys as a precaution even if no keys have been
+ * lost or stolen.
  *
  * The private keys are always encrypted with a hash of the user's password.
+ * This is the passwordHash.
  *
- * Keys are never removed from this file. It is an append-only log of all unique
- * identity keys.
+ * Keys are never removed from this file. If you want to rotate your keys, then
+ * create a new key and stop using the old ones. However, there is no reason to
+ * delete keys. This file should never be too long.
  *
- * There is always one keyfile per user per domain.
+ * There is always at least one keyfile per user per domain.
  *
- * It is absolutely not intended that this file be used to store all keys or all
- * paymails held by the user. If the user has other keys, they should be
- * encrypted using keys from this file or from keys derived from keys in this
- * file.
- *
- * [
- *   {paymail, pubkeyHash, passwordHashHash, encryptedPrivKey},
- *   ...
- * ]
- *
- * This file is extensible. We enforce that it is an array. If it is ever an
- * object, that is because there is a new standard that needs new fields that
- * are not provided by the array.
+ * It is absolutely not intended that this file be used to store all keys held
+ * by the user. If the user has other keys, they should be encrypted using keys
+ * from this file or from keys derived from keys in this file.
+ * 
+ * The keyfile is extensible. 
  *
  * The user never sees the keys. It is assumed that every user has at least one
  * reliable 2FF (Two Factor Friend) which can be used to recover the keys.
+ * 
+ * We do not use bcrypt. It is important that the password is strong. If you
+ * want a stronger password for your users, then use bcrypt on their password
+ * and use that as the password for the keyfile.
+ * 
+ * If the user has had their password compromised, then the keys in this file
+ * can no longer be trusted. What they should do in that case is to change their
+ * password first and then generate a new identity to be added to this file.
+ * They should then migrate all old keys to the new keys. e.g., if they have
+ * money stored in derivative keys, they should move all that to new keys.
+ * 
+ * File structure:
+ *
+ * {
+ *   keysByPasswordHashHash: {
+ *     [passwordHashHash]: {
+ *       [addressStr]: [encryptedPrivKey],
+ *       [addressStr]: [encryptedPrivKey],
+ *       ...
+ *     },
+ *     [passwordHashHash]: {
+ *       [addressStr]: [encryptedPrivKey],
+ *       [addressStr]: [encryptedPrivKey],
+ *       ...
+ *     },
+ *     ...
+ *   }
+ * }
+ *
  */
 'use strict'
 
-import { Struct, Hash, Ach, Address } from '@openspv/lib'
+import { Struct, Hash, Ach, Address, PrivKey, PubKey } from '@openspv/lib'
 
 class Keyfile extends Struct {
   constructor () {
-    this.keys = []
+    super()
+    this.keysByPasswordHashHash = {}
   }
 
   static passwordHashFromPassword (password = '') {
@@ -51,46 +74,88 @@ class Keyfile extends Struct {
   static passwordHashHashFromPassword (password = '') {
     // the passwordHashHash is used to identify the password and is written to
     // the keyfile
-    const passwordHash = this.constructor.passwordHashFromPassword(password)
+    const passwordHash = Keyfile.passwordHashFromPassword(password)
     return Hash.sha256Hmac(Buffer.from(passwordHash, 'hex'), Buffer.from('KeyFilePasswordHashHash')).toString('hex')
   }
 
-  static encryptKey (privKey, passwordHash) {
+  static encryptPrivKey (privKey, password = '', ivBuf) {
+    const passwordHash = Keyfile.passwordHashFromPassword(password)
+    return Keyfile.encryptPrivKeyWithPasswordHash(privKey, passwordHash, ivBuf)
+  }
+
+  static encryptPrivKeyWithPasswordHash (privKey, passwordHash, ivBuf) {
     // the private key is encrypted with the passwordHash
     const passwordHashBuf = Buffer.from(passwordHash, 'hex')
     const privKeyBuf = privKey.toBuffer()
     const messageBuf = privKeyBuf
     const cipherKeyBuf = passwordHashBuf
-    return Ach.encrypt(messageBuf, cipherKeyBuf).toString('hex')
+    return Ach.encrypt(messageBuf, cipherKeyBuf, ivBuf).toString('hex')
   }
 
-  static decryptKey (encryptedPrivKey, passwordHash) {
+  static decryptPrivKey (encryptedPrivKey, password = '', ivBuf) {
+    const passwordHash = Keyfile.passwordHashFromPassword(password)
+    return Keyfile.decryptPrivKeyWithPasswordHash(encryptedPrivKey, passwordHash, ivBuf)
+  }
+
+  static decryptPrivKeyWithPasswordHash (encryptedPrivKey, passwordHash) {
     const encBuf = Buffer.from(encryptedPrivKey, 'hex')
     const cipherKeyBuf = Buffer.from(passwordHash, 'hex')
-    return Ach.decrypt(encBuf, cipherKeyBuf).toString('hex')
+    const privKeyBuf = Ach.decrypt(encBuf, cipherKeyBuf)
+    const privKey = PrivKey.fromBuffer(privKeyBuf)
+    return privKey
   }
 
-  pushNewKey (paymail, keyPair, password = '') {
-    const pubKey = keyPair.pubKey
-    const privKey = keyPair.privKey
-    const pubKeyHash = Address.fromPubKey(pubKey).toHex()
-    const passwordHash = this.constructor.passwordHashFromPassword(password)
-    const passwordHashHash = this.constructor.passwordHashHashFromPassword(password)
-    const encryptedPrivKey = this.constructor.encryptedKey(privKey, passwordHash)
-    this.keys.push({ paymail, pubKeyHash, passwordHashHash, encryptedPrivKey })
+  addPrivKey (privKey, password = '', ivBuf) {
+    const pubKey = PubKey.fromPrivKey(privKey)
+    const address = Address.fromPubKey(pubKey)
+    const addressStr = address.toString()
+    const encryptedPrivKey = Keyfile.encryptPrivKey(privKey, password, ivBuf)
+    if (!this.keysByPasswordHashHash) {
+      this.keysByPasswordHashHash = {}
+    }
+    const passwordHashHash = Keyfile.passwordHashHashFromPassword(password)
+    if (!this.keysByPasswordHashHash[passwordHashHash]) {
+      this.keysByPasswordHashHash[passwordHashHash] = {}
+    }
+    this.keysByPasswordHashHash[passwordHashHash][addressStr] = encryptedPrivKey
+    return address
+  }
+
+  getPrivKey (address, password = '', ivBuf) {
+    const addressStr = address.toString()
+    const passwordHashHash = Keyfile.passwordHashHashFromPassword(password)
+    const encryptedPrivKey = this.keysByPasswordHashHash[passwordHashHash][addressStr]
+    const privKey = Keyfile.decryptPrivKey(encryptedPrivKey, password, ivBuf)
+    return privKey
+  }
+
+  changePassword (oldPassword = '', newPassword = '', ivBuf) {
+    const oldPasswordHashHash = Keyfile.passwordHashHashFromPassword(oldPassword)
+    if (!this.keysByPasswordHashHash[oldPasswordHashHash]) {
+      throw new Error('no keys with that password')
+    }
+    const addressStrings = Object.keys(this.keysByPasswordHashHash[oldPasswordHashHash])
+    const obj = {}
+    for (const addressStr of addressStrings) {
+      const address = Address.fromString(addressStr)
+      const privKey = this.getPrivKey(address, oldPassword)
+      this.addPrivKey(privKey, newPassword, ivBuf)
+    }
+    delete this.keysByPasswordHashHash[oldPasswordHashHash]
+    const newPasswordHashHash = Keyfile.passwordHashHashFromPassword(newPassword)
+    // return number of keys re-encrypted
+    return Object.keys(this.keysByPasswordHashHash[newPasswordHashHash]).length
   }
 
   fromJSON (json) {
-    const keys = JSON.parse(JSON.stringify(json))
-    if (!Array.isArray(this.keys)) {
-      throw new Error('keys must be an array')
-    }
-    this.keys = keys
+    this.keysByPasswordHashHash = json.keysByPasswordHashHash
     return this
   }
 
   toJSON () {
-    return JSON.parse(JSON.stringify(this.keys))
+    return {
+      keysByPasswordHashHash: JSON.parse(JSON.stringify(this.keysByPasswordHashHash))
+    }
   }
 }
 
--- private/openspv/keyfile/test/index.mjs
+++ private/openspv/keyfile/test/index.mjs
@@ -1,8 +1,139 @@
 import { Keyfile } from '../src/index.mjs'
+import { PrivKey, Bn } from '@openspv/lib'
 import should from 'should'
 
 describe('Keyfile', () => {
   it('should open', () => {
     should.exist(Keyfile)
+  })
+
+  describe('@passwordHashFromPassword', () => {
+    it('should hash to this value', () => {
+      const password = 'password'
+      const passwordHash = Keyfile.passwordHashFromPassword(password)
+      passwordHash.should.equal('e0c979c8c5cf3df1806d8b3ff56ac4afa7d58df2fa5315bef159261d393fa0be')
+    })
+  })
+
+  describe('@passwordHashHashFromPassword', () => {
+    it('should hash to this value', () => {
+      const password = 'password'
+      const passwordHash = Keyfile.passwordHashHashFromPassword(password)
+      passwordHash.should.equal('59d3451b5f37c2519189977c9a2581822be60c278c065596e56f534810c32345')
+    })
+  })
+
+  describe('@encryptPrivKey', () => {
+    it('should get this value', () => {
+      const ivBuf = Buffer.from('06'.repeat(16), 'hex')
+      const privKey = PrivKey.fromBn(new Bn(500))
+      const password = 'password'
+      const encryptedPrivKey = Keyfile.encryptPrivKey(privKey, password, ivBuf)
+      encryptedPrivKey.should.equal('44de80ba381723e7f846ea6c01cea71e65fb3471b3338d282c94cada8ee53a290606060606060606060606060606060640f1341010a05e2f947ca84dab18ca154338f56a9b39c96624a94e2d5cc9420d926ee849ba16d90c2feb048b2c547090')
+    })
+
+    it('should encrypt and decrypt without ivBuf', () => {
+      const privKey = PrivKey.fromRandom()
+      const password = 'password'
+      const encryptedPrivKey = Keyfile.encryptPrivKey(privKey, password)
+      const privKey2 = Keyfile.decryptPrivKey(encryptedPrivKey, password)
+      privKey2.toString().should.equal(privKey.toString())
+    })
+  })
+  
+  describe('@decryptPrivKey', () => {
+    it('should get this value', () => {
+      const ivBuf = Buffer.from('06'.repeat(16), 'hex')
+      const privKey = PrivKey.fromBn(new Bn(500))
+      const password = 'password'
+      const encryptedPrivKey = Keyfile.encryptPrivKey(privKey, password, ivBuf)
+      encryptedPrivKey.should.equal('44de80ba381723e7f846ea6c01cea71e65fb3471b3338d282c94cada8ee53a290606060606060606060606060606060640f1341010a05e2f947ca84dab18ca154338f56a9b39c96624a94e2d5cc9420d926ee849ba16d90c2feb048b2c547090')
+      const privKey2 = Keyfile.decryptPrivKey(encryptedPrivKey, password, ivBuf)
+      privKey2.toString().should.equal(privKey.toString())
+    })
+  })
+
+  describe('#addPrivKey', () => {
+    it('should add a privKey', () => {
+      const privKey = PrivKey.fromRandom()
+      const password = 'password'
+      const keyfile = new Keyfile()
+      keyfile.addPrivKey(privKey, password)
+      Object.keys(keyfile.keysByPasswordHashHash).length.should.equal(1)
+    })
+
+    it('should add a privKey and get the privKey back again', () => {
+      const privKey = PrivKey.fromRandom()
+      const password = 'password'
+      const keyfile = new Keyfile()
+      const address = keyfile.addPrivKey(privKey, password)
+      const privKey2 = keyfile.getPrivKey(address, password)
+      privKey2.toString().should.equal(privKey.toString())
+    })
+
+    it('should add a privKey and get exactly this data back again', () => {
+      const privKey = PrivKey.fromBn(new Bn(501))
+      const ivBuf = Buffer.from('07'.repeat(16), 'hex')
+      const password = 'p4ssw0rd'
+      const keyfile = new Keyfile()
+      const address = keyfile.addPrivKey(privKey, password, ivBuf)
+      const jsonStr = JSON.stringify(keyfile.toJSON())
+      jsonStr.should.equal('{"keysByPasswordHashHash":{"41d2e53673e5b99ad93d8bbffcb9d2eb6dc088c09fe6cda7ed0dbb868edad793":{"186bbzwCAj6CcJqpB6og1Amifg2DGupMqn":"27bf795dcd74a11e0ca2e78701f0b5bdb621853621c20682eeb9df7564d071e407070707070707070707070707070707b44caf6a9b6bdc403171d646b775886c92be29b44f09c32ce80d170fd8600ce0bfd515d372711234b52f41aa5f85ac9b"}}}')
+    })
+  })
+
+  describe('#changePassword', () => {
+    it('should add a privKey and then change the password and then get the key', () => {
+      const privKey = PrivKey.fromRandom()
+      const password1 = 'password 1'
+      const keyfile = new Keyfile()
+      const address = keyfile.addPrivKey(privKey, password1)
+      Object.keys(keyfile.keysByPasswordHashHash).length.should.equal(1)
+      const password2 = 'password 2'
+      const number = keyfile.changePassword(password1, password2)
+      number.should.equal(1)
+      const privKey2 = keyfile.getPrivKey(address, password2)
+      privKey2.toString().should.equal(privKey.toString())
+    })
+
+    it('should add two privKeys and then change the password and then get the keys', () => {
+      const privKey1 = PrivKey.fromRandom()
+      const privKey2 = PrivKey.fromRandom()
+      const password1 = 'password 1'
+      const keyfile = new Keyfile()
+      const address1 = keyfile.addPrivKey(privKey1, password1)
+      const address2 = keyfile.addPrivKey(privKey2, password1)
+      Object.keys(keyfile.keysByPasswordHashHash).length.should.equal(1)
+      const password2 = 'password 2'
+      const number = keyfile.changePassword(password1, password2)
+      number.should.equal(2)
+      const newPrivKey1 = keyfile.getPrivKey(address1, password2)
+      newPrivKey1.toString().should.equal(privKey1.toString())
+      const newPrivKey2 = keyfile.getPrivKey(address2, password2)
+      newPrivKey2.toString().should.equal(privKey2.toString())
+    })
+  })
+
+  describe('#toJSON / @fromJSON', () => {
+    it('should go to and from json', () => {
+      const privKey1 = PrivKey.fromRandom()
+      const privKey2 = PrivKey.fromRandom()
+      const password1 = 'password 1'
+      const keyfile = new Keyfile()
+      const address1 = keyfile.addPrivKey(privKey1, password1)
+      const address2 = keyfile.addPrivKey(privKey2, password1)
+      Object.keys(keyfile.keysByPasswordHashHash).length.should.equal(1)
+      const password2 = 'password 2'
+      const number = keyfile.changePassword(password1, password2)
+      number.should.equal(2)
+      const newPrivKey1 = keyfile.getPrivKey(address1, password2)
+      newPrivKey1.toString().should.equal(privKey1.toString())
+      const newPrivKey2 = keyfile.getPrivKey(address2, password2)
+      newPrivKey2.toString().should.equal(privKey2.toString())
+
+      const json = keyfile.toJSON()
+      const keyfile2 = Keyfile.fromJSON(json)
+      JSON.stringify(keyfile2.toJSON()).should.equal(JSON.stringify(json))
+    })
   })
 })
