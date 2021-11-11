5408d15f1a67d83fdd2e09e48389bb4368d28fa8
Ryan X. Charles <ryan@coasian.com>
2021-11-11 12:15:53 -0800

    align left on iOS

--- private/rxc/app/Components/TopBar.js
+++ private/rxc/app/Components/TopBar.js
@@ -2,22 +2,11 @@ import * as React from "react";
 import { Appbar } from "react-native-paper";
 import { View } from 'react-native'
 
-// import { Image } from 'react-native'
-
-// const fs = require('fs')
-
-// const base64 = fs.readFileSync('../static/rxc-header-2-dark.png')
-// const data = `data:image/png;base64,${base64}`
-
-// const TopBar = () => (
-//   <Appbar.Header style={{backgroundColor: '#4d4843'}}>
-//     <Image source={data} width={475} height={100} />
-//   </Appbar.Header>
-// );
-
 const TopBar = () => (
   <Appbar.Header style={{backgroundColor: '#4d4843'}}>
-    <Appbar.Content title="Ryan X. Charles" subtitle="The limits are all in your head." color='#f8f3e7' />
+    <View style={{textAlign: 'left'}}>
+      <Appbar.Content title="Ryan X. Charles" subtitle="The limits are all in your head." color='#f8f3e7' />
+    </View>
   </Appbar.Header>
 );
 
