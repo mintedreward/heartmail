5964a106cc1a9daac834046e39626157c8510161
Ryan X. Charles <ryan@coasian.com>
2021-11-16 14:26:59 -0800

    use capital letters for page title

--- rxc-app/Screens/MainScreen.js
+++ rxc-app/Screens/MainScreen.js
@@ -24,11 +24,11 @@ const Screen = () => {
   return (
     <NavigationContainer linking={linking}>
       <Tab.Navigator activeColor='#f8f3e7' inactiveColor='#c5c0b6' barStyle={{ backgroundColor: '#4d4843' }}>
-        <Tab.Screen name='articles' component={ArticlesScreen} options={{ tabBarLabel: 'Articles', tabBarIcon: 'facebook' }} />
-        <Tab.Screen name='videos' component={VideosScreen} options={{ tabBarLabel: 'Videos', tabBarIcon: 'youtube' }} />
-        <Tab.Screen name='headlines' component={HeadlinesScreen} options={{ tabBarLabel: 'Headlines', tabBarIcon: 'twitter' }} />
-        <Tab.Screen name='images' component={ImagesScreen} options={{ tabBarLabel: 'Images', tabBarIcon: 'instagram' }} />
-        <Tab.Screen name='privacy' component={PrivacyPolicyScreen} options={{ tabBarLabel: 'Videos', tabBarIcon: 'file-outline' }} />
+        <Tab.Screen name='articles' component={ArticlesScreen} options={{ title: 'Articles', tabBarLabel: 'Articles', tabBarIcon: 'facebook' }} />
+        <Tab.Screen name='videos' component={VideosScreen} options={{ title: 'Videos', tabBarLabel: 'Videos', tabBarIcon: 'youtube' }} />
+        <Tab.Screen name='headlines' component={HeadlinesScreen} options={{ title: 'Headlines', tabBarLabel: 'Headlines', tabBarIcon: 'twitter' }} />
+        <Tab.Screen name='images' component={ImagesScreen} options={{ title: 'Images', tabBarLabel: 'Images', tabBarIcon: 'instagram' }} />
+        <Tab.Screen name='privacy' component={PrivacyPolicyScreen} options={{ title: 'Privacy', tabBarLabel: 'Privacy', tabBarIcon: 'file-outline' }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
