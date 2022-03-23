6c1433ea81378e86b553deb2184220ee176ef29b
Ryan X. Charles <ryan@heartmail.com>
2022-03-23 12:33:52 -0500

    testing redirect

--- btb-web/pages/_middleware.js
+++ btb-web/pages/_middleware.js
@@ -6,9 +6,12 @@ export default function redirectWWW (req, ev) {
   const url = new URL(req.url)
   const hostname = url.hostname
   if (process.env.DOMAIN_PREFIX) {
-    if (!hostname.startsWith(process.env.DOMAIN_PREFIX + '.')) {
-      url.hostname = `${process.env.DOMAIN_PREFIX}.${hostname}`
+    console.log('domain prefix')
+    if (!hostname.startsWith(process.env.DOMAIN_PREFIX)) {
+      console.log('starts with')
+      url.hostname = `${process.env.DOMAIN_PREFIX}${hostname}`
       const href = url.href
+      console.log('redirect', href)
       return NextResponse.redirect(href)
     }
   }
