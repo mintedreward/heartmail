874d20c980f1ecf92801d2c737986ef56e6706a8
Ryan X. Charles <ryan@heartmail.com>
2022-03-24 13:10:51 -0500

    linkbsv: serve all front-end files

---
+++ linkbsv-web/server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var express = require('express')
+var app = express()
+
+app.use('/', express.static('build'))
+app.use('/*', express.static('build'))
+
+app.listen(3000)
+
+console.log('Listening on port 3000')
+
