48c99df1c02769ea957f70a6723b9df5ba1c4959
Ryan X. Charles <ryan@heartmail.com>
2022-03-25 14:26:52 -0500

    use components

--- btb-web/pages/index.js
+++ btb-web/pages/index.js
@@ -2,6 +2,55 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+function Title () {
+  return (
+    <div className={styles.logoheader}>
+      <Image src="/btb.png" alt="BeTheBroadcast.com: Own your social media." width="393" height="41" />
+    </div>
+  )
+}
+
+function Motto () {
+  return (
+    <div className={styles.content}>
+      <h1>Own Your Social Media</h1>
+      <p>The MVP will launch in April. Please email your name and personal website to Ryan, Casey and Diddy to get the launch announcement and other updates.</p>
+    </div>
+  )
+}
+
+function TeamMember ({name, email, title, avatar}) {
+  return (
+    <div className={styles.teamColumn}>
+      <div className={styles.contactCard}>
+        <div className={styles.contactCardRow}>
+          <div className={styles.contactCardImage}>
+            <Image className={styles.contactCardImageImage} src={avatar} alt={name} width="100" height="100" />
+          </div>
+          <div className={styles.contactCardText}>
+            <div className={styles.contactCardName}>{name}</div>
+            {title}<br />
+            {email}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Team () {
+  return (
+    <div className={styles.team}>
+      <div className={styles.teamRow}>
+        <TeamMember name={'Ryan X. Charles'} email={'ryan@heartmail.com'} title={'Cofounder & CEO'} avatar={'/ryan.jpg'} />
+        <TeamMember name={'Casey N. Hamilton'} email={'casey@heartmail.com'} title={'Cofounder & COO'} avatar={'/casey.jpg'} />
+        <TeamMember name={'Diddy Wheldon'} email={'diddy@heartmail.com'} title={'Project Manager'} avatar={'/diddy.jpg'} />
+        <TeamMember name={'Ruth Heasman'} email={'ruth@heartmail.com'} title={'Design & UI/UX'} avatar={'/ruth.jpg'} />
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,75 +61,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.logoheader}>
-            <Image src="/btb.png" alt="BeTheBroadcast.com: Own your social media." width="393" height="41" />
-        </div>
 
-        <div className={styles.content}>
-          <h1>Own Your Social Media</h1>
-          <p>The MVP will launch in April. Please email your name and personal website to Ryan, Casey and Diddy to get the launch announcement and other updates.</p>
-        </div>
+        <Title />
 
-        <div className={styles.cofounders}>
-          <div className={styles.cofoundersRow}>
-            <div className={styles.cofoundersColumn}>
-              <div className={styles.contactCard}>
-                <div className={styles.contactCardRow}>
-                  <div className={styles.contactCardImage}>
-                    <Image className={styles.contactCardImageImage} src="/ryan.jpg" alt="Ryan X. Charles" width="100" height="100" />
-                  </div>
-                  <div className={styles.contactCardText}>
-                    <div className={styles.contactCardName}>Ryan X. Charles</div>
-                    Cofounder &amp; CEO<br />
-                    ryan@heartmail.com
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.cofoundersColumn}>
-              <div className={styles.contactCard}>
-                <div className={styles.contactCardRow}>
-                  <div className={styles.contactCardImage}>
-                    <Image className={styles.contactCardImageImage} src="/casey.jpg" alt="Casey N. Hamilton" width="100" height="100" />
-                  </div>
-                  <div className={styles.contactCardText}>
-                    <div className={styles.contactCardName}>Casey N. Hamilton</div>
-                    Cofounder &amp; COO<br />
-                    casey@heartmail.com
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.cofoundersColumn}>
-              <div className={styles.contactCard}>
-                <div className={styles.contactCardRow}>
-                  <div className={styles.contactCardImage}>
-                    <Image className={styles.contactCardImageImage} src="/diddy.jpg" alt="Diddy Wheldon" width="100" height="100" />
-                  </div>
-                  <div className={styles.contactCardText}>
-                    <div className={styles.contactCardName}>Diddy Wheldon</div>
-                    Project Manager<br />
-                    diddy@heartmail.com
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.cofoundersColumn}>
-              <div className={styles.contactCard}>
-                <div className={styles.contactCardRow}>
-                  <div className={styles.contactCardImage}>
-                    <Image className={styles.contactCardImageImage} src="/ruth.jpg" alt="Ruth Heasman" width="100" height="100" />
-                  </div>
-                  <div className={styles.contactCardText}>
-                    <div className={styles.contactCardName}>Ruth Heasman</div>
-                    Branding &amp; UI/UX<br />
-                    ruth@heartmail.com
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Motto />
+
+        <Team />
+
       </main>
     </div>
   )
