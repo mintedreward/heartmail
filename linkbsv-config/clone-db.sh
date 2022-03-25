69f3fc4e5a0a6f9cc4064c504466bd6029ef0bd8
Ryan X. Charles <ryan@heartmail.com>
2022-03-25 12:17:08 -0500

    remove linkbsv-config

--- linkbsv-config/clone-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo Dumping from their database. Enter their DB password.
-mongodump mongodb+srv://cluster0-linkbsv.qc87z.mongodb.net/linkBsv -u linkBsv-backend-server
-
-echo Restoring to our database. Enter our DB password.
-mongorestore --host linkbsv-docdb.cluster-cbtzb5eylgfi.us-west-2.docdb.amazonaws.com:27017 --username linkbsv --drop
-
