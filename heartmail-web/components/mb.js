a960db4deff54da69c093574ae444451840c3d60
Ryan X. Charles <ryan@heartmail.com>
2022-05-05 12:11:35 -0500

    update button style

--- heartmail-web/components/mb.js
+++ heartmail-web/components/mb.js
@@ -3,7 +3,7 @@ import MoneyButton from '@moneybutton/react-money-button'
 export default function MB () {
   return (
     <div style={{width: 300, height: 70}}>
-      <div style={{width: 290, height: 60, padding: 4, margin: 5, backgroundColor: '#f6f6f6', borderRadius: 30, border: '1px solid #66ACE2'}}>
+      <div style={{width: 280, height: 50, padding: 0, margin: 10, backgroundColor: '#f6f6f6', borderRadius: 25}}>
         <MoneyButton
           to='heartmail@moneybutton.com'
           amount='1'
