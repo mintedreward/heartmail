6de959eba4c8479d42a2c2eb2110e0bd06e93027
Ryan X. Charles <ryan@heartmail.com>
2022-05-06 10:33:39 -0500

    working on landing page

---
+++ heartmail-web/pages/landing.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Landing.module.css'
+import MB from '../components/mb.js'
+
+export default function Landing() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>HeartMail</title>
+        <meta name="description" content="Get paid per email." />
+        <link rel="icon" href="/heartmail-avatar-light.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoheader}>
+            <Image src="/heartmail.png" alt="HeartMail: Get paid per email." width="350" height="60" />
+        </div>
+        <br /><br />
+        <MB />
+      </main>
+    </div>
+  )
+}
