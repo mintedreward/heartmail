7c7d37b85d49a5d0aff4702c920effb1e77cb237
Ryan X. Charles <ryan@heartmail.com>
2022-05-06 12:25:12 -0500

    material ui button - it works

--- heartmail-web/pages/landing.js
+++ heartmail-web/pages/landing.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Landing.module.css'
 import MB from '../components/mb.js'
+import Button from '@mui/material/Button';
 
 export default function Landing() {
   return (
@@ -13,8 +14,9 @@ export default function Landing() {
 
       <main className={styles.main}>
         <div className={styles.logoheader}>
-            <Image src="/heartmail.png" alt="HeartMail: Get paid per email." width="350" height="60" />
+          <Image src="/heartmail.png" alt="HeartMail: Get paid per email." width="350" height="60" />
         </div>
+        <Button variant="contained">Hello World</Button>
         <br /><br />
         <MB />
       </main>
