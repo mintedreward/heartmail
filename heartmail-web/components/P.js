592076bb6b95cf3908fbfdd4139d442d4faa8c01
Ryan X. Charles <ryan@heartmail.com>
2022-05-08 16:40:37 -0500

    P tags look the same as real p tags

--- heartmail-web/components/P.js
+++
@@ -1,9 +0,0 @@
-import Typography from '@mui/material/Typography'
-
-export default function P (props) {
-  return (
-    <Typography variant="p" component="p" mb="1em">
-      {props.children}
-    </Typography>
-  )
-}
--- heartmail-web/pages/landing.js
+++ heartmail-web/pages/landing.js
@@ -2,7 +2,6 @@ import MB from '../components/MB.js'
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Layout from '../components/Layout.js'
-import P from '../components/P.js'
 
 export default function Landing () {
   return (
@@ -14,20 +13,20 @@ export default function Landing () {
       <Typography variant="h2" component="h2" mb={'50px'} sx={{textAlign: 'center'}}>
         per email
       </Typography>
-      <P>
+      <p>
         HeartMail is email with a pay wall. Charge advertisers a contact fee to send you an email. Whitelist your friends so they don’t have to pay.
-      </P>
-      <P>
+      </p>
+      <p>
         Random heartmails are free. Custom heartmails, such as [name]@heartmail.com, cost $1.00. You can have as many heartmails as you want and you can resell heartmails for any amount of money.
-      </P>
-      <P>
+      </p>
+      <p>
         HeartMail launches July 15, 2022. An early access version will launch June 15, 2022.
         Join early to get privileged access to your custom heartmails.
         Early access users will be invited in the order they register.
-      </P>
-      <P>
+      </p>
+      <p>
         By buying early access, you agree to the Terms of Service.
-      </P>
+      </p>
       <MB />
     </Layout>
   )
