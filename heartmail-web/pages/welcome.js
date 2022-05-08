6986cf9e57fbbad1b80af05fd9bd063a7e16280f
Ryan X. Charles <ryan@heartmail.com>
2022-05-08 16:59:29 -0500

    about h1. add welcome page.

--- heartmail-web/pages/welcome.js
+++ heartmail-web/pages/welcome.js
@@ -1,11 +1,10 @@
 import Layout from '../components/Layout.js'
 import H1 from '../components/H1.js'
 
-
 export default function Landing() {
   return (
-    <Layout title='About'>
-      <H1>About</H1>
+    <Layout title='Welcome'>
+      <H1>Welcome</H1>
     </Layout>
   )
 }
