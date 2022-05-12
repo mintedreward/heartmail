0e80d985406ff2182c5bf1424670ac283bdbd5e2
Ryan X. Charles <ryan@heartmail.com>
2022-05-12 10:42:55 -0500

    fix deploy

--- deploy-heartmail-web.zsh
+++ deploy-heartmail-web.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# make sure we are in the btb source directory
+# make sure we are in the heartmail source directory
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 
@@ -10,11 +10,11 @@ source .env.local
 # get the latest commit hash as the docker version number
 version=`git rev-parse --verify HEAD`
 
-cd $dir/heartmail-web
+# cd $dir/heartmail-web
 
 echo Building heartmail-web
 
-docker build --build-arg NPM_TOKEN=${NPM_TOKEN} . -t heartmail-web --platform linux/amd64
+docker build -f Dockerfile-heartmail-web . -t heartmail-web --platform linux/amd64
 docker tag heartmail-web ryanxcharles/heartmail-web:${version}
 docker push ryanxcharles/heartmail-web:${version}
 
