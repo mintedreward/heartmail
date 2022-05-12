dc73de5eadf05f1da3ccbba0f0c44d3bd0adc8ea
Ryan X. Charles <ryan@heartmail.com>
2022-05-12 17:11:06 -0500

    wording

--- heartmail-web/pages/terms.js
+++ heartmail-web/pages/terms.js
@@ -9,7 +9,11 @@ export default function Terms () {
       <p>You are the custodian of your money, not us.</p>
       <p>You are responsible for paying your taxes, not us.</p>
       <p>You must not use HeartMail to publish obscene content. If you do, we have the right to delete your account, which may cause your assets to become irrecoverable.</p>
-      <p>If you give people an affiliate link then you are an affiliate. Affiliates must continue to help the people they onboard to sign in and use the service or we have the right to terminate the affiliate relationship.</p>
+      <p>
+        If you give people an affiliate link and they become a paying user then you are an affiliate.
+        Affiliates earn 20% of HeartMail's revenue for that user.
+        Affiliates must continue to help the users they onboard to use HeartMail or we have the right to terminate the affiliate relationship.
+      </p>
       <p>You have the right to delete your account at any time. If you choose to delete your account, you must first transfer your assets to other users, or your assets may become irrecoverable.</p>
       <p>We have the right to update the Terms of Service at any time. We may ask you to agree to the updated Terms of Service. You may be prohibited from using HeartMail until you agree.</p>
     </Layout>
