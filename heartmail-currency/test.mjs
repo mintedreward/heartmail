1cf1cabf04a91b60a7896dfdb26c7a648d88eac9
Ryan X. Charles <ryan@heartmail.com>
2022-05-12 08:41:27 -0500

    add test package for currency library

---
+++ heartmail-currency/test.mjs
@@ -0,0 +1,9 @@
+'use strict'
+import * as currency from './index.mjs'
+import should from 'should'
+
+describe('currency', () => {
+  it('should exist', () => {
+    should.exist(currency)
+  })
+})
