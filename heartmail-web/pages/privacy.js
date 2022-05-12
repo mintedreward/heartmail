8d03849edf1a6b37b5a3534f09c061d5ee79946f
Ryan X. Charles <ryan@heartmail.com>
2022-05-12 05:23:09 -0500

    privacy policy and terms of service

--- heartmail-web/pages/privacy.js
+++ heartmail-web/pages/privacy.js
@@ -1,10 +1,13 @@
 import Layout from '../components/Layout.js'
 import PageTitle from '../components/PageTitle.js'
 
-export default function Landing() {
+export default function Landing () {
   return (
     <Layout title='Privacy Policy'>
       <PageTitle>Privacy Policy</PageTitle>
+      <p>We do not sell your data to third parties without your permission.</p>
+      <p>We use cookies and local storage to track user sessions inside HeartMail. We may use cookies and local storage to track user sessions across the internet with your permission.</p>
+      <p>You have the right to delete your data at any time.</p>
     </Layout>
   )
 }
--- heartmail-web/pages/terms.js
+++ heartmail-web/pages/terms.js
@@ -5,6 +5,12 @@ export default function Terms () {
   return (
     <Layout title='Terms of Service'>
       <PageTitle>Terms of Service</PageTitle>
+      <p>You are a user of HeartMail. We are HeartMail Inc.</p>
+      <p>You are the custodian of your money, not us.</p>
+      <p>You are responsible for paying your taxes, not us.</p>
+      <p>If you give people an affiliate link then you are an affiliate for us. Affiliates must continue to help the people they onboard sign in and use the service or we have the right to terminate the affiliate relationship.</p>
+      <p>You have the right to delete your account at any time. If you choose to delete your account, you must first transfer your assets to other users, or your assets may become irrecoverable.</p>
+      <p>We have the right to update the Terms of Service at any time. We may ask you to agree to the updated Terms of Service. You may be prohibited from using HeartMail until you agree.</p>
     </Layout>
   )
 }
