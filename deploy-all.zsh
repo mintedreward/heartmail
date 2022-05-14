a29d3f520ea8dcd4f92422be923fbf50aea5e5c9
Ryan X. Charles <ryan@heartmail.com>
2022-05-14 12:34:08 -0500

    remove obsolete webpages

--- deploy-all.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-./deploy-coasian-web.zsh
-./deploy-heartmail-web.zsh
-./deploy-heartx-web.zsh
-./deploy-itisbit-web.zsh
-./deploy-openspv-web.zsh
-./deploy-rxc-app.zsh
-./deploy-tob-web.zsh
