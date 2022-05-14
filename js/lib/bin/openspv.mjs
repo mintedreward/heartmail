cc174b615977fd71929e5929937378eee90b68af
Ryan X. Charles <ryan@heartmail.com>
2022-05-14 12:13:37 -0500

    latest version

--- js/lib/bin/openspv.mjs
+++ js/lib/bin/openspv.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
-const repl = require('repl')
-const OpenSPV = require('../')
+import repl from 'repl'
+import * as OpenSPV from '../index.mjs'
 // Make all SPV classes globally available.
 Object.assign(global, OpenSPV, { OpenSPV: OpenSPV })
 repl.start({
