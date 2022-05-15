a816917f3b7f412241344d9ebbf82ede78220a77
Ryan X. Charles <ryan@heartmail.com>
2022-05-14 19:58:47 -0500

    dockerfile

--- heartmail-web/components/MoneyButtonReferralNeeded.js
+++ heartmail-web/components/MoneyButtonReferralNeeded.js
@@ -8,7 +8,7 @@ export default function MoneyButtonReferralNeeded (props) {
         <div style={{ width: 280, height: 50, padding: 0, margin: 10, backgroundColor: '#f6f6f6', borderRadius: 25 }}>
           <Box sx={{ paddingTop: '12px', flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Box>
-              <SupervisedUserCircle sx={{ height: '25px', width: '25px', marginRight: '3px', float: 'left', color: 'info.main' }} />
+              <SupervisedUserCircle sx={{ height: '25px', width: '25px', marginRight: '3px', float: 'left', color: 'primary.main' }} />
               {props.label}
             </Box>
           </Box>
