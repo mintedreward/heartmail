f0d9af6a66198d03999639453a21e1a8ef65691d
Ryan X. Charles <ryan@heartmail.com>
2022-05-16 04:09:02 -0500

    import fonts locally. goodbye google.

--- heartmail-web/pages/_document.js
+++ heartmail-web/pages/_document.js
@@ -5,10 +5,6 @@ export default function Document () {
   return (
     <Html>
       <Head>
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-        />
         <link rel='icon' href='/heartmail-avatar-light.png' />
       </Head>
       <body>
