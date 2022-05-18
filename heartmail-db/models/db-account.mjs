3a967dbd86061a5e9214c94d1851570865486a66
Ryan X. Charles <ryan@heartmail.com>
2022-05-18 09:42:54 -0500

    progress on account

--- heartmail-db/models/db-account.mjs
+++ heartmail-db/models/db-account.mjs
@@ -1,5 +1,40 @@
 import DbKey from './db-key.mjs'
 
 export default class DbAccount extends DbKey {
+  constructor (...args) {
+    super(...args)
+    this.typeStr = 'account'
+  }
 
+  fromJSON (json) {
+    super.fromObject(json)
+  }
+
+  fromProperties (accessGrantedAt, ownerEmailAddress, paymentEmailAddress, affiliateKeyAlias, contactFeeAmountUsd) {
+    const obj = {
+      accessGrantedAt,
+      ownerEmailAddress,
+      paymentEmailAddress,
+      affiliateKeyAlias: affiliateKeyAlias.toString(),
+      contactFeeAmountUsd
+    }
+    const objStr = JSON.stringify(obj)
+    const objBuf = Buffer.from(objStr)
+    this.dataBuf = objBuf
+    return this
+  }
 }
+
+/*
+-create table if not exists heartmail.accounts (
+-    keyAlias text,
+-    accessGrantedAt timestamp,
+-    ownerEmailAddress text,
+-    paymentEmailAddress text,
+-    affiliateKeyAlias text,
+-    contactFeeAmountUsd float,
+-    createdAt timestamp,
+-    updatedAt timestamp,
+-    primary key (keyAlias)
+-);
+*/
