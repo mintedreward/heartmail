20806f0e2e5b5390033e6bb1a4a50a2293bd7dc3
Ryan X. Charles <ryan@heartmail.com>
2022-05-18 11:09:37 -0500

    parse data buf

--- heartmail-db/models/db-account.mjs
+++ heartmail-db/models/db-account.mjs
@@ -1,3 +1,4 @@
+import { KeyAlias } from 'heartmail-lib'
 import DbKey from './db-key.mjs'
 
 export default class DbAccount extends DbKey {
@@ -6,8 +7,32 @@ export default class DbAccount extends DbKey {
     this.typeStr = 'account'
   }
 
-  fromJSON (json) {
-    super.fromObject(json)
+  createDataBuf () {
+    const dataObj = {
+      accessGrantedAt: this.accessGrantedAt,
+      ownerEmailAddress: this.ownerEmailAddress,
+      paymentEmailAddress: this.paymentEmailAddress,
+      affiliateKeyAlias: this.affiliateKeyAlias,
+      contactFeeAmountUsd: this.contactFeeAmountUsd
+    }
+    const dataStr = JSON.stringify(dataObj)
+    const dataBuf = Buffer.from(dataStr)
+    this.dataBuf = dataBuf
+    return this
+  }
+
+  parseDataBuf () {
+    const dataStr = this.dataBuf.toString()
+    const dataJSON = JSON.parse(dataStr)
+    const dataObj = {
+      accessGrantedAt: new Date(dataJSON.accessGrantedAt),
+      ownerEmailAddress: dataJSON.ownerEmailAddress,
+      paymentEmailAddress: dataJSON.paymentEmailAddress,
+      affiliateKeyAlias: KeyAlias.fromJSON(dataJSON.affiliateKeyAlias),
+      contactFeeAmountUsd: dataJSON.contactFeeAmountUsd
+    }
+    this.fromObject(dataObj)
+    return this
   }
 
   fromProperties (accessGrantedAt, ownerEmailAddress, paymentEmailAddress, affiliateKeyAlias, contactFeeAmountUsd) {
--- heartmail-db/test/db-account.mjs
+++ heartmail-db/test/db-account.mjs
@@ -1,6 +1,6 @@
 /* global describe,it */
 import DbAccount from '../models/db-account.mjs'
-import { KeyAddress, PubKey, KeyAlias } from 'heartmail-lib'
+import { KeyAddress, PubKey, PrivKey, KeyAlias, Bn } from 'heartmail-lib'
 import should from 'should'
 
 describe('DbAccount', () => {
@@ -41,6 +41,57 @@ describe('DbAccount', () => {
       json.paymentEmailAddress.should.equal('name@example.com')
       json.affiliateKeyAlias.should.equal(dbAccount.affiliateKeyAlias.toJSON())
       json.contactFeeAmountUsd.should.equal(1.00)
+    })
+  })
+
+  describe('#createDataBuf', () => {
+    it('should make a dataBuf from these properties', () => {
+      const dbAccount = DbAccount.fromRandom()
+      const privKey = PrivKey.fromBn(new Bn(5))
+      const pubKey = PubKey.fromPrivKey(privKey)
+      const keyAddress = KeyAddress.fromPubKey(pubKey)
+      const keyAlias = KeyAlias.fromKeyAddress(keyAddress)
+      dbAccount.fromObject({
+        accessGrantedAt: new Date('2022-05-18T15:40:34.015Z'),
+        ownerEmailAddress: 'name@example.com',
+        paymentEmailAddress: 'name@example.com',
+        affiliateKeyAlias: keyAlias,
+        contactFeeAmountUsd: 1.00
+      })
+      dbAccount.createDataBuf()
+      should.exist(dbAccount.dataBuf)
+      dbAccount.dataBuf.toString('hex').should.equal('7b226163636573734772616e7465644174223a22323032322d30352d31385431353a34303a33342e3031355a222c226f776e6572456d61696c41646472657373223a226e616d65406578616d706c652e636f6d222c227061796d656e74456d61696c41646472657373223a226e616d65406578616d706c652e636f6d222c22616666696c696174654b6579416c696173223a225c2234377834646d3435737477347736766c3464696664687a74725c22222c22636f6e74616374466565416d6f756e74557364223a317d')
+    })
+  })
+
+  describe('#parseDataBuf', () => {
+    it('should make these properties from this databuf', () => {
+      const dbAccount = DbAccount.fromRandom()
+      const privKey = PrivKey.fromBn(new Bn(5))
+      const pubKey = PubKey.fromPrivKey(privKey)
+      const keyAddress = KeyAddress.fromPubKey(pubKey)
+      const keyAlias = KeyAlias.fromKeyAddress(keyAddress)
+      dbAccount.fromObject({
+        accessGrantedAt: new Date('2022-05-18T15:40:34.015Z'),
+        ownerEmailAddress: 'name@example.com',
+        paymentEmailAddress: 'name@example.com',
+        affiliateKeyAlias: keyAlias,
+        contactFeeAmountUsd: 1.00
+      })
+      dbAccount.createDataBuf()
+      should.exist(dbAccount.dataBuf)
+      dbAccount.dataBuf.toString('hex').should.equal('7b226163636573734772616e7465644174223a22323032322d30352d31385431353a34303a33342e3031355a222c226f776e6572456d61696c41646472657373223a226e616d65406578616d706c652e636f6d222c227061796d656e74456d61696c41646472657373223a226e616d65406578616d706c652e636f6d222c22616666696c696174654b6579416c696173223a225c2234377834646d3435737477347736766c3464696664687a74725c22222c22636f6e74616374466565416d6f756e74557364223a317d')
+
+      const dbAccount2 = DbAccount.fromRandom()
+      dbAccount2.fromObject({ dataBuf: dbAccount.dataBuf })
+      dbAccount2.parseDataBuf()
+      dbAccount2.privKey.toString().should.not.equal(dbAccount.dataBuf.toString())
+      dbAccount2.dataBuf.toString('hex').should.equal(dbAccount.dataBuf.toString('hex'))
+      dbAccount2.accessGrantedAt.toJSON().should.equal(dbAccount.accessGrantedAt.toJSON())
+      dbAccount2.ownerEmailAddress.should.equal(dbAccount.ownerEmailAddress)
+      dbAccount2.paymentEmailAddress.should.equal(dbAccount.paymentEmailAddress)
+      dbAccount2.affiliateKeyAlias.toJSON().should.equal(dbAccount.affiliateKeyAlias.toJSON())
+      dbAccount2.contactFeeAmountUsd.should.equal(dbAccount.contactFeeAmountUsd)
     })
   })
 })
