cc58ee8a46b517a65e47201380e2d619b96e9edd
Ryan X. Charles <ryan@heartmail.com>
2022-05-19 09:21:26 -0500

    hard link environment variables for next.js

--- heartmail-web/components/MoneyButtonBuyEarly.js
+++ heartmail-web/components/MoneyButtonBuyEarly.js
@@ -1,13 +1,35 @@
 import MoneyButton from '@moneybutton/react-money-button'
-import MoneyButtonReferralNeeded from '../components/MoneyButtonReferralNeeded'
+// import MoneyButtonReferralNeeded from '../components/MoneyButtonReferralNeeded'
 
 export default function MoneyButtonBuyEarly (props) {
-  const affiliateEmail2 = props.affiliateEmail2
+  const affiliate = props.affiliate
+  let outputs = []
 
-  if (affiliateEmail2 === '') {
-    return (
-      <MoneyButtonReferralNeeded label='You need a referral.' />
-    )
+  if (affiliate.hasAffiliate) {
+    outputs = [
+      {
+        to: 'heartmail@moneybutton.com',
+        amount: '8',
+        currency: 'USD'
+      },
+      {
+        to: affiliate.externalPaymail,
+        amount: '2',
+        currency: 'USD'
+      }
+    ]
+  } else {
+    outputs = [
+      {
+        to: 'heartmail@moneybutton.com',
+        amount: '10',
+        currency: 'USD'
+      }
+    ]
+  }
+
+  const handlePayment = (payment) => {
+    console.log(payment); console.log(payment.amountUsd, payment.id, payment.userId, payment.senderEmail2, payment.user.email, payment.user.emailVerified)
   }
 
   return (
@@ -15,23 +37,12 @@ export default function MoneyButtonBuyEarly (props) {
       <div style={{ width: 300, height: 70 }}>
         <div style={{ width: 280, height: 50, padding: 0, margin: 10, backgroundColor: '#f6f6f6', borderRadius: 25 }}>
           <MoneyButton
-            outputs={[
-              {
-                to: 'heartmail@moneybutton.com',
-                amount: '8',
-                currency: 'USD'
-              },
-              {
-                to: { affiliateEmail2 },
-                amount: '2',
-                currency: 'USD'
-              }
-            ]}
+            outputs={outputs}
             label='Buy Early'
             clientIdentifier='c3a2ca70156b6cdf2ce3e860ba2bef13'
             buttonId='Buy Early'
             buttonData='Buy Early'
-            onPayment={(payment) => { console.log(payment); console.log(payment.amountUsd, payment.id, payment.userId, payment.senderEmail2, payment.user.email, payment.user.emailVerified) }}
+            onPayment={handlePayment}
           />
         </div>
       </div>
