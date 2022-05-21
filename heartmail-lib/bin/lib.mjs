5796472792a89d7e4fc806779484c309b8943c0c
Ryan X. Charles <ryan@heartmail.com>
2022-05-21 11:51:03 -0500

    💌HeartMailLib

---
+++ heartmail-lib/bin/lib.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import repl from 'repl'
+import * as HeartMailLib from '../index.mjs'
+Object.assign(global, HeartMailLib, { HeartMailLib })
+repl.start({
+  prompt: '💌lib> ',
+  useGlobal: true,
+  ignoreUndefined: true
+})
--- heartmail-lib/test/index.mjs
+++ heartmail-lib/test/index.mjs
@@ -1,9 +1,9 @@
 /* global describe,it */
 import should from 'should'
-import * as OpenSPVLib from '../src/index.mjs'
+import * as HeartMailLib from '../src/index.mjs'
 
-describe('OpenSPVLib', function () {
+describe('HeartMailLib', function () {
   it('should pass this sanity check on loading the main package', function () {
-    should.exist(OpenSPVLib)
+    should.exist(HeartMailLib)
   })
 })
