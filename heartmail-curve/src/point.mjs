145238f0e60757cbfb4b6c5c31a20ddf2b93f606
Ryan X. Charles <ryan@heartmail.com>
2022-05-23 05:12:00 -0500

    prepare resources for implementing Point

--- heartmail-curve/src/point.mjs
+++ heartmail-curve/src/point.mjs
@@ -1,3 +1,10 @@
 export default class Point {
 
+  // point addition
+  // https://cryptohack.org/courses/elliptic/ecc1/
+  // point multiplication
+  // https://cryptohack.org/courses/elliptic/ecc2/
+  // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+  // https://stackoverflow.com/questions/63656794/flush-reload-cache-side-channel-attack
+
 }
