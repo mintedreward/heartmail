a18c993b7388b99900a16755d380df800eb195da
Ryan X. Charles <ryan@heartmail.com>
2022-05-26 14:26:27 -0500

    new logo. add ruth as branding.

--- heartmail-web/pages/about.js
+++ heartmail-web/pages/about.js
@@ -17,9 +17,10 @@ export default function AboutPage () {
         Houston, TX 77043<br />
         USA
       </p>
-      <ContactCard avatar='/ryan.jpg' name='Ryan X. Charles' heartmail='ryan@heartmail.com' bio='Cofounder & CEO of HeartMail' />
-      <ContactCard avatar='/casey.jpg' name='Casey N. Hamilton' heartmail='casey@heartmail.com' bio='Cofounder & COO of HeartMail' />
-      <ContactCard avatar='/diddy.jpg' name='Diddy Wheldon' heartmail='diddy@heartmail.com' bio='Project Manager of HeartMail' />
+      <ContactCard avatar='/ryan.jpg' name='Ryan X. Charles' heartmail='ryan@heartmail.com' bio='Cofounder & CEO' />
+      <ContactCard avatar='/casey.jpg' name='Casey N. Hamilton' heartmail='casey@heartmail.com' bio='Cofounder & COO' />
+      <ContactCard avatar='/diddy.jpg' name='Diddy Wheldon' heartmail='diddy@heartmail.com' bio='Project Manager' />
+      <ContactCard avatar='/ruth.jpg' name='Ruth Heasman' heartmail='ruth@heartmail.com' bio='Branding' />
       <MoneyButtonTip />
     </Layout>
   )
