d0d582d3208f089d30fbc35ed031a3934fa541d5
Ryan X. Charles <ryan@heartmail.com>
2022-05-27 09:59:19 -0500

    fix text color on contact card

--- heartmail-web/components/ContactCard.js
+++ heartmail-web/components/ContactCard.js
@@ -1,6 +1,7 @@
 import Card from '@mui/material/Card'
 import Avatar from '@mui/material/Avatar'
 import Link from '../components/Link'
+import Typography from '@mui/material/Typography'
 
 export default function ContactCard (props) {
   const name = props.name
@@ -12,7 +13,7 @@ export default function ContactCard (props) {
       <Avatar src={avatar} sx={{ float: 'left', width: 100, height: 100, marginRight: '10px' }} />
       <b>{name}</b><br />
       <Link href={`mailto:${heartmail}`}>{heartmail}</Link><br />
-      {bio}
+      <Typography variant='body1' component='span' sx={{ color: 'text.secondary' }}>{bio}</Typography>
     </Card>
   )
 }
