d96cb547579252b65b7ac2174c2794ca7740b5ec
Ryan X. Charles <ryan@heartmail.com>
2022-05-29 08:58:24 -0500

    address input box

--- heartmail-web/pages/addresses.js
+++ heartmail-web/pages/addresses.js
@@ -2,6 +2,9 @@ import Layout from '../components/Layout'
 import PageTitle from '../components/PageTitle'
 import ContactCard from '../components/ContactCard'
 import SelfTabs from '../components/SelfTabs'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 
 export default function AddressesPage () {
   return (
@@ -9,6 +12,13 @@ export default function AddressesPage () {
       <PageTitle>Addresses</PageTitle>
       <ContactCard avatar='/casey.jpg' name='Casey N. Hamilton' heartmail='casey@heartmail.com' bio='Cofounder & COO of HeartMail' />
       <SelfTabs value={1} />
+      <p>
+        You can register any [alias]@moneybutton.com you own for free for a limited time.
+      </p>
+      <TextField id='outlined-basic' label='New Address' defaultValue='[alias]@heartmail.com' variant='outlined' sx={{ width: '100%' }} />
+      <Box mt='10px' sx={{ textAlign: 'right' }}>
+        <Button variant='contained'>Register</Button>
+      </Box>
     </Layout>
   )
 }
