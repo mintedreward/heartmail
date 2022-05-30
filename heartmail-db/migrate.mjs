b3b435dba97eece5d675f4f95874560599754f15
Ryan X. Charles <ryan@heartmail.com>
2022-05-30 13:19:54 -0500

    new database migration. not tested yet.

--- heartmail-db/migrate.mjs
+++ heartmail-db/migrate.mjs
@@ -2,24 +2,97 @@ import { getClient, useKeyspace } from './connect.mjs'
 const client = getClient()
 const keyspace = process.env.HEARTMAIL_DB_KEYSPACE
 
-const query = `
-create table if not exists ${keyspace}.keys (
-    key_alias_left bigint,
-    key_alias_right bigint,
-    key_address text,
-    pub_key text,
-    priv_key text,
-    type_str text,
-    data_buf blob,
-    created_at timestamp,
-    updated_at timestamp,
-    primary key (key_alias_left, key_alias_right)
-)
-`
+const queries = [
+`create table if not exists ${keyspace}.keys (
+  key_alias_left bigint,
+  key_alias_right bigint,
+  key_address text,
+  pub_key text,
+  priv_key text,
+  type_str text,
+  data_buf blob,
+  created_at timestamp,
+  updated_at timestamp,
+  primary key (key_alias_left, key_alias_right)
+)`,
+`create table if not exists ${keyspace}.accounts (
+  id text,
+  priv_key text,
+
+  created_at timestamp,
+  updated_at timestamp,
+  signed_in_at timestamp,
+
+  name text,
+  primary_heartmail_address text,
+  bio text,
+  contact_fee_amount_usd double,
+  affiliate_id text,
+
+  access_granted_at timestamp,
+  mb_payment_id text,
+  mb_txid text,
+  mb_email text,
+  mb_paymail text,
+  mb_identity_key text,
+
+  primary key (id)
+)`,
+`create table if not exists ${keyspace}.mb_paymail_accounts (
+  mb_paymail text,
+
+  created_at timestamp,
+  updated_at timestamp,
+  signed_in_at timestamp,
+
+  account_id text,
+  account_name text,
+  account_primary_heartmail_address text,
+  account_bio text,
+  account_contact_fee_amount_usd double,
+  account_affiliate_id text,
+
+  primary key (mb_paymail, signed_in_at)
+) with clustering order by (signed_in_at desc)`,
+`create table if not exists ${keyspace}.heartmail_addresses (
+  heartmail_address text,
+
+  created_at timestamp,
+  updated_at timestamp,
+
+  account_id text,
+
+  primary key (heartmail_address)
+)`,
+`create table if not exists ${keyspace}.account_heartmail_addresses (
+  account_id text,
+
+  heartmail_address text,
+
+  created_at timestamp,
+  updated_at timestamp,
+
+  primary key (account_id, updated_at)
+) with clustering order by (updated_at desc)`,
+`create table if not exists ${keyspace}.account_avatars (
+  account_id text,
+
+  avatar_120px blob,
+  avatar_288px blob,
+
+  created_at timestamp,
+  updated_at timestamp,
+  primary key (account_id)
+)`
+]
 
 ;(async function () {
   await useKeyspace()
-  await client.execute(query)
+  for (let i = 0; i < queries.length; i++) {
+    if (queries[i]) {
+      await client.execute(queries[i])
+    }
+  }
   console.log('Migration complete.')
   await client.shutdown()
 })()
