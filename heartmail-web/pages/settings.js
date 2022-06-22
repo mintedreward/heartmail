a43db0e1a301d6381c8d4a19280e5fdd9eb034c8
Ryan X. Charles <ryan@heartmail.com>
2022-06-22 12:18:31 -0500

    settings page displays correct values except avatar

--- heartmail-web/pages/settings.js
+++ heartmail-web/pages/settings.js
@@ -10,41 +10,48 @@ import Card from '@mui/material/Card'
 import Avatar from '@mui/material/Avatar'
 import SignOut from '../components/SignOut'
 import { withSessionSsr } from '../lib/session'
+import dbApi from 'heartmail-db/db-api/db-api'
 
 export const getServerSideProps = withSessionSsr(
-  function getServerSideProps ({ req }) {
+  async function getServerSideProps ({ req }) {
+    const accountId = req.session?.accountId
     const email = req.session?.email
 
-    if (!email) {
+    if (!accountId || !email) {
       return {
         notFound: true
       }
     }
 
+    const account = await dbApi.getAccount(accountId)
+    const accountJSON = account.toJSON()
+
     return {
       props: {
+        account: accountJSON,
         email
       }
     }
   }
 )
 
-export default function SettingsPage () {
+export default function SettingsPage (props) {
+  const { account, email } = props
   return (
     <Layout title='Settings' account={null}>
       <PageTitle>Settings</PageTitle>
-      <ContactCard avatar='/casey.jpg' name='Casey N. Hamilton' heartmail='casey@heartmail.com' bio='Cofounder & COO of HeartMail' />
+      <ContactCard avatar='/casey.jpg' name={account.name} heartmail={account.heartmail} bio={account.bio} />
       <SelfTabs value={2} />
       <Card variant='outlined' sx={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '16px', marginTop: '8px', marginBottom: '8px', borderColor: 'action.disabled' }}>
         <Avatar src='/casey.jpg' sx={{ width: 96, height: 96 }} />
       </Card>
-      <TextField id='name' label='Name' defaultValue='Casey N. Hamilton' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
-      <TextField id='bio' label='Bio' defaultValue='Cofounder & COO of HeartMail' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
+      <TextField id='name' label='Name' defaultValue={account.name} variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
+      <TextField id='bio' label='Bio' defaultValue={account.bio} variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
       <TextField id='contactFeeAmountUsd' label='Contact Fee' defaultValue='$1.00' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
-      <TextField id='mbEmail' disabled label='External Email' defaultValue='casey@bethebroadcast.com' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
-      <TextField id='mbPaymail' disabled label='External Paymail' defaultValue='bethebroadcast@moneybutton.com' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
-      <TextField id='mbUserId' disabled label='Money Button User ID' defaultValue='12345' variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
-      <AffiliateCard heartmail='casey@heartmail.com' />
+      <TextField id='mbEmail' disabled label='External Email' defaultValue={account.email} variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
+      <TextField id='mbPaymail' disabled label='External Paymail' defaultValue={account.paymail} variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
+      <TextField id='mbUserId' disabled label='Authentication Email' defaultValue={email} variant='outlined' sx={{ width: '100%', marginTop: '8px', marginBottom: '8px' }} />
+      <AffiliateCard heartmail={account.heartmail} />
       <Box sx={{ textAlign: 'right', marginTop: '16px', marginBottom: '16px' }}>
         <Button disabled variant='text' ml='16px'>Delete account</Button>
         <Box sx={{ display: 'inline-block', width: '8px' }} />
