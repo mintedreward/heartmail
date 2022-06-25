daf705004a89e349c33e1d7bd7125cf9e502acef
Ryan X. Charles <ryan@heartmail.com>
2022-06-25 09:01:21 -0500

    redirect access page to sign in

--- heartmail-web/next.config.js
+++ heartmail-web/next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   optimizeFonts: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async redirects () {
+    return [
+      {
+        source: '/access/:id',
+        destination: '/sign-in',
+        permanent: false
+      }
+    ]
+  }
 }
