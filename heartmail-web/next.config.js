60adb86705eb8d0f4ea93213df27f8f58986b002
Ryan X. Charles <ryan@heartmail.com>
2022-07-30 05:46:44 -0500

    bsvalias redirect

--- heartmail-web/next.config.js
+++ heartmail-web/next.config.js
@@ -8,6 +8,11 @@ module.exports = {
         source: '/access/:id',
         destination: '/sign-in',
         permanent: false
+      },
+      {
+        source: '/.well-known/bsvalias',
+        destination: '/.well-known/api-capabilities',
+        permanent: false
       }
     ]
   }
